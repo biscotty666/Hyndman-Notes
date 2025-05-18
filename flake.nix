43ecd89ff2ea4a3a1fd0f1f9d7ee1f9cfcf59826
biscotty666 <biscotty666@gmail.com>
2025-05-18 10:33:08 -0600

    Added flake; began refactor;

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+      #packageOverrides = pkgs.callPackage ./python-packages.nix {};
+      #python3 = pkgs.python3.override { inherit packageOverrides; };
+    in {
+      devShell = pkgs.mkShell {
+        name = "python-venv";
+        venvDir = "./.venv";
+        buildInputs = with pkgs; [
+            R
+            rPackages.pagedown
+            rPackages.quarto
+            rPackages.fpp3
+            rPackages.tidyverse
+            rPackages.latex2exp
+            rPackages.GGally
+            chromium
+            pandoc
+            texlive.combined.scheme-full
+            rstudio
+            quarto
+        ];
+
+        shellHook = ''
+            export BROWSER=zen
+            #jupyter lab
+        '';
+
+      };
+    }
+  );
+}
